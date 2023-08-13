eb798abe441991d2da70bc17897c82aa8faaa8fd
LakshanJayaweera426 <127861567+LakshanJayaweera426@users.noreply.github.com>
2023-08-13 08:58:11 -0700

    Add files via upload

---
+++ HumanResourceManagement/InsertData/RegistrationSuccsefull.php
@@ -0,0 +1,65 @@
+<?php require_once('connection.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<title>ICTS - Technology</title>
+
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css">
+
+	<link rel="stylesheet" type="text/css" href="style2.css">
+
+	<!-- Logo for web browser titile bar -->
+	<link rel="icon" href="logo.png">
+
+</head>
+<body class="body-home">
+
+	<div class="black-fill">
+		<div class="container"> <br/><br/>
+			<section id="form"
+					 class="d-flex justify-content-center align-items-center flex-column">
+				
+				<form>
+					<div class="col-md-4">
+					      	<img src="logo.png" class="img-fluid rounded-start">
+					</div>
+
+					<div>
+						<img src="1681140618738.png" class="successimg">
+					</div>
+					<h3>Registration is Successful</h3>
+				</form>
+
+
+			</section>
+				
+			
+			
+
+			
+				
+				
+			
+
+		<div class="text-center text-light">
+				Copyright &copy; 2023 University of Vavuniya. All right reserved.
+			</div>
+
+		</div>
+		
+	</div>
+
+
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"></script>
+
+
+	
+
+	
+</body>
+</html>
+
---
+++ HumanResourceManagement/InsertData/form-connection2.php
@@ -0,0 +1,10 @@
+
+
+
+<?php
+
+	header('location:https://drive.google.com/drive/folders/1seNa82IORwtzTyUBFX92JvG1Eoz6IOxT');
+
+
+		
+?>
